f63fe2dbecfdffd3ea19527f646ad84d828ea434
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-08-02 06:18:35 +0100

    got album artwork from DB styled & appended to UI

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php include("inc/header.php"); ?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+<div class="gridViewContainer">
+    <?php
+    // Connect to SQL DB and select albums by random
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+    // Loop through album DB table & output values
+    while ($row = mysqli_fetch_array($albumQuery)) {
+        echo "<div class='gridViewItem'>
+                <a href='album.php?id=" . $row['id'] . "'>
+            <img src='" . $row['artworkPath'] . "'>
+            <div class='gridViewInfo'>" . $row['title'] .
+            "</div>
+                </a>
+            </div>";
+    }
+    ?>
+</div>
+
+
+</div>
+
+
+
+
 <?php include("inc/footer.php"); ?>
