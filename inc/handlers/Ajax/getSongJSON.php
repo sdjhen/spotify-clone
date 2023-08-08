6c13cf77d3d09341fe7840b8f82b76b612b9ed81
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-08-08 17:51:52 +0100

    retrieved song from DB without refresh using Ajax call

--- inc/handlers/Ajax/getSongJSON.php
+++ inc/handlers/Ajax/getSongJSON.php
@@ -1,2 +1,13 @@
 <?php
-echo 'Hello';
+include("../../config.php");
+
+// Handle Ajax call
+if (isset($_POST['songID'])) {
+    $songID = $_POST['songID'];
+    // Query Songs ID from Songs Table DB
+    $query = mysqli_query($con, "SELECT * FROM Songs WHERE id='$songID'");
+    // Convert query data to array
+    $resultArray = mysqli_fetch_array($query);
+    // Covert results to JSON & respond to Ajax call 
+    echo json_encode($resultArray);
+}
