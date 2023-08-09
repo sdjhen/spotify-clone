b29402ab90c7b07cf424755957fe96cdd042fa1f
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-08-09 01:11:13 +0100

    fixed time formatting issue in formatTime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,10 @@ const formatTime = (seconds) => {
   const minutes = Math.floor(roundedSeconds / 60); // Rounds down
   const remainingSeconds = roundedSeconds - minutes * 60;
 
-  return minutes + `:` + remainingSeconds;
+  const formattedSeconds =
+    remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+
+  return `${minutes}:${formattedSeconds}`;
 };
 
 // Audio Player
