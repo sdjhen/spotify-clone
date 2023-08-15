431d1c62cc808ca6129303baa37e0772de8a3f7f
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-08-15 22:15:54 +0100

    redirected index.php to browse.php

--- index.php
+++ index.php
@@ -2,24 +2,6 @@
 include("inc/includedFiles.php");
 ?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
-<div class="gridViewContainer">
-    <?php
-    // Connect to SQL DB and select albums by random
-    $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-    // Loop through album DB table & output values
-    while ($row = mysqli_fetch_array($albumQuery)) {
-        echo "<div class='gridViewItem'>
-                 <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
-            <img src='" . $row['artworkPath'] . "'>
-            <div class='gridViewInfo'>" . $row['title'] .
-            "</div>
-                </span>
-            </div>";
-    }
-    ?>
-</div>
-
-
-</div>
+<script>
+    openPage("browse.php")
+</script>
